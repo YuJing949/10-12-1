854daa171b464136d9169ae9b06ec4a02d143ca9
YuJing949 <Zoesyj1130@outlook.com>
2024-12-10 15:48:51 +0000

    SHAPE

--- sketch.js
+++ sketch.js
@@ -1,9 +1,24 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(600, 600);
 }
 
 function draw() {
-  background(220);
+  background(255);
 
-  circle(width / 2, height / 2, (width / 2 - mouseX) * 2);
+  //circle(width / 2, height / 2, (width / 2 - mouseX) * 2);
+
+  stroke(0);
+  fill(0);
+
+  beginShape();
+
+  vertex(mouseX, mouseY);
+
+  //bezierVertex( mouseX, mouseY, mouseY, mouseX, 350, 400);
+  bezierVertex( mouseY, mouseX, 350, 400, mouseY, mouseX, mouseX, mouseY, 200, 200);
+
+  endShape(CLOSE);
+
+  
 }
+
